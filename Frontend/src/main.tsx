fd8579c280edb12391839e8c0fce80515d6f68cf
juan jose botero garcía <jjboterog@eafit.edu.co>
2025-08-30 13:07:00 -0500

    feat: add catalog link and place it in the navbar

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import HomePage from './pages/HomePage'
+import Catalog from './pages/Catalog'
 import App from './App'
 import { Link } from 'react-router-dom' 
 
@@ -14,6 +15,10 @@ const router = createBrowserRouter([
       { // Pagina principal
         path: '/',
         element: <HomePage/>
+      },
+      { // Pagina del catalogo de analisis
+        path: '/catalog',
+        element: <Catalog/> 
       },
     ],
     ErrorBoundary: () => {
