6f54596b4240c6c7b2e5bcc8b69834b91affe51a
JuanJoseBotero <juanjosebg2@gmail.com>
2025-10-06 19:59:31 -0500

    style: Make the homepage responsive

--- Frontend/src/pages/HomePage.tsx
+++ Frontend/src/pages/HomePage.tsx
@@ -3,25 +3,27 @@ import { Link } from "react-router-dom";
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col items-center h-screen space-y-8">
-      <div className="background-color-home flex flex-row w-full h-dvw items-center justify-around">
-        <div className="flex flex-col space-y-10 w-2/5 ">
-          <h1 className="text-8xl font-bold">
+    <div className="flex flex-col items-center space-y-8 bg-white">
+      <div className="background-color-home resposive-big-container flex flex-wrap justify-between items-center min-h-dvh">
+        <div className="flex flex-col space-y-10 w-full sm:w-2/5 items-center sm:items-start sm:text-left text-center">
+          <h1 className="heading-extrabig font-bold">
             AI-powered intelligent market analysis
           </h1>
-          <h2 className="text-3xl">
+          <h2 className="heading-2">
             Explore data, spot opportunities, and make decisions with insight.
           </h2>
           <Link to="/about" className="main-button button-text">
             Learn more
           </Link>
         </div>
-        <div className="w-130 h-130">
-          <DotLottieReact
-            src="https://lottie.host/45c511b2-0fa0-4806-a155-89beed5bfa2a/hQyIWuxp4R.lottie"
-            loop
-            autoplay
-          />
+        <div className=" w-full sm:w-auto flex justify-center">
+          <div className=" w-70 h-70 md:w-90 md:h-90 lg:w-110 lg:h-110 xl:w-130 xl:h-130">
+            <DotLottieReact
+              src="https://lottie.host/45c511b2-0fa0-4806-a155-89beed5bfa2a/hQyIWuxp4R.lottie"
+              loop
+              autoplay
+            />
+          </div>
         </div>
       </div>
     </div>
